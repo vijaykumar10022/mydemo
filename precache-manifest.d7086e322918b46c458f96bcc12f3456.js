0424c38ea8fdaa83877b8a12294aacb9680d1016
vijaykumar <vijay10022@gmail.com>
2020-04-20 12:38:52 +0530

    Updates

---
+++ precache-manifest.d7086e322918b46c458f96bcc12f3456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b5e568435ab5120ba3ca5cdfbaa8f63",
+    "url": "/mydemo/index.html"
+  },
+  {
+    "revision": "354d719b2374dfd9644b",
+    "url": "/mydemo/static/css/main.524d044f.chunk.css"
+  },
+  {
+    "revision": "374317c1b6f1bda7dfdd",
+    "url": "/mydemo/static/js/2.8e03b53f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mydemo/static/js/2.8e03b53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "354d719b2374dfd9644b",
+    "url": "/mydemo/static/js/main.309f36bd.chunk.js"
+  },
+  {
+    "revision": "3daff5a65cdd3fa61a1c",
+    "url": "/mydemo/static/js/runtime-main.476e6261.js"
+  }
+]);
